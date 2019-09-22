7f280c36c0639b53c36b0ccedec1d6012eabcd81
Marc Philipp <marc@gradle.com>
2019-09-22 16:19:26 +0200

    Remove leaked dependency

--- cli/src/main/java/org/example/cli/App.java
+++ cli/src/main/java/org/example/cli/App.java
@@ -1,6 +1,5 @@
 package org.example.cli;
 
-import com.google.common.collect.ImmutableSet;
 import org.example.lib.Library;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +8,7 @@ public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        var strings = ImmutableSet.copyOf(new Library().someLibraryMethod());
+        var strings = new Library().someLibraryMethod();
         LOG.debug("strings = {}", strings);
         System.out.println(strings);
     }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     api(platform(project(":platform")))
     implementation("org.slf4j:slf4j-api")
-    api("com.google.guava:guava")
+    implementation("com.google.guava:guava")
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
